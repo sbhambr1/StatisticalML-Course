a2aa7a629b322947ab07b374c6b491f6dc220669
sbhambr1 <sbhambr1@asu.edu>
2021-11-09 12:53:04 -0700

    corrected backtrack algo

--- backtrack_line_search.py
+++ backtrack_line_search.py
@@ -2,28 +2,29 @@ import numpy as np
 alpha = 0.4
 beta = 0.9
 
-f = lambda x: (x[0]**2 + (5*x[1]**2))
+f = lambda x: (x[0]**2 + 5*(x[1]**2))
 dfx1 = lambda x: (2*x[0])
 dfx2 = lambda x: (10*x[1])
 
-t=1
-count=1
+t = 1
+count = 1
 x0 = np.array([5,1])
-dx0 = np.array([10,10])
+dx0 = np.array([10, 10])
+
 
 def backtrack(x0, dfx1, dfx2, t, alpha, beta, count):
-    while (f(x0) - f(x0-t*np.array([dfx1(x0),dfx2(x0)]))+alpha*t*np.dot(np.array([dfx1(x0),dfx2(x0)]),np.array([dfx1(x0),dfx2(x0)])))<0:
-        t*=beta
+    while (f(x0) - (f(x0 - t*np.array([dfx1(x0), dfx2(x0)])) + alpha * t * np.dot(np.array([dfx1(x0), dfx2(x0)]), np.array([dfx1(x0), dfx2(x0)])))) < 0:
+        t *= beta
         print("""
-        ##########################
-        ###### iteration {} ######
-        ##########################
-        """.format(count))
-        print("inequality: ", f(x0)-f(x0-t*np.array([dfx1(x0),dfx2(x0)])) + alpha*t*np.dot(np.array([dfx1(x0),dfx2(x0)]), np.array([dfx1(x0),dfx2(x0)])))
-        count += 1
 
+########################
+###   iteration {}   ###
+########################
+        """.format(count))
+        print("Inequality: ",  f(x0) - (f(x0 - t*np.array([dfx1(x0), dfx2(x0)])) + alpha * t * np.dot(np.array([dfx1(x0), dfx2(x0)]), np.array([dfx1(x0), dfx2(x0)]))))
+        count += 1
     return t
 
-t = backtrack(x0,dfx1,dfx2,t,alpha,beta,count)
+t = backtrack(x0, dfx1, dfx2, t, alpha, beta,count)
 
-print("\nfinal step size: ", t)
+print("\nfinal step size :",  t)
